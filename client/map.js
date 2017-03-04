f68fa5eb1c08a1a555cc295f6aa05c79ab37abf0
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2017-03-04 17:05:52 +0100

    first map with

--- client/map.js
+++ client/map.js
@@ -19,6 +19,8 @@ var vbz_marker_options = {
     fillOpacity: 0.8
 };
 
+var timeNow = 35010;
+
 $.ajax({
     dataType: "json",
     url: "stops.geojson",
@@ -38,9 +40,65 @@ $.ajax({
             vbz_stops[stop_code] = jsonFeature;
         });
 
-        console.log(vbz_stops);
+        $.ajax({
+            dataType: "json",
+            url: "routes?date=2016-07-01&now=" + timeNow,
+            success: loadTrips,
+            error: function(jqXHR, textStatus, errorThrown) {
+                debugger;
+            }
+        });
     },
     error: function(jqXHR, textStatus, errorThrown) {
         debugger;
     }
 });
+
+
+function loadTrips(data) {
+    // data = data.slice(0, 1);
+    for (var trip of data) {
+        for (var idx = 0; idx < trip.segments.length; idx++) {
+            var segment = trip.segments[idx];
+
+            var vbzStopA = vbz_stops[segment.from_stop_code];
+            var vbzStopB = vbz_stops[segment.to_stop_code];
+
+            var isLastStop = idx === (trip.segments.length - 1);
+
+            var depA = segment.from_time_actual;
+            var depB;
+            if (isLastStop) {
+                depB = segment.to_time_actual;
+            } else {
+                var nextSegment = trip.segments[idx + 1];
+                depB = nextSegment.from_time_actual;
+            }
+
+            if ((timeNow >= depA) && (timeNow <= depB)) {
+                var arrB = segment.to_time_actual;
+                let zvv_line = trip.zvv_line;
+                if (timeNow <= arrB) {
+                    var timeAC = timeNow - depA;
+                    var timeAB = segment.to_time_actual - depA;
+                    var ratio = timeAC / timeAB;
+                    var coordX = vbzStopA.geometry.coordinates[0] + (vbzStopB.geometry.coordinates[0] - vbzStopA.geometry.coordinates[0]) * ratio;
+                    var coordY = vbzStopA.geometry.coordinates[1] + (vbzStopB.geometry.coordinates[1] - vbzStopA.geometry.coordinates[1]) * ratio;
+
+                    var featureCoords = [coordY, coordX];
+                    var marker = L.marker(featureCoords).addTo(map).bindPopup(function(){
+                        return zvv_line;
+                    });
+                } else {
+                    var featureCoords = vbzStopB.geometry.coordinates;
+                    var marker = L.marker([featureCoords[1], featureCoords[0]]).addTo(map).bindPopup(function(){
+                        return zvv_line;
+                    });
+                }
+            }
+        }
+            
+    }
+}
+
+
